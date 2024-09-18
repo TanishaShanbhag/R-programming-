993456e9dcb2c375ae26f598b1672e02d06b1cb8
TanishaShanbhag <tanishan.23.beai@acharya.ac.in>
2024-09-18 09:50:13 +0530

    Add files via upload

---
+++ Survey.R
@@ -0,0 +1,5 @@
+cat("Tanisha Narayan Shanbhag\n")
+cat("1AY23AI130")
+vec=c("G","B","A","B","G","A","A","B","G","A")
+f=factor(vec,levels=c("G","A","B"),labels=c("Good","Average","Bad"),ordered=TRUE)
+f
